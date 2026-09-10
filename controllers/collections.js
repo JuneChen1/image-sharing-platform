1cb0346a1c1a53ad7fb4aca069e3d8fe6520fe6e
JuneChen1 <june850713@gmail.com>
2026-09-10 13:12:25 +0800

    feat(collections): add photo to collection route

--- controllers/collections.js
+++ controllers/collections.js
@@ -1,3 +1,4 @@
+const { IsNull } = require('typeorm');
 const { isValidString, isValidUUID } = require('../utils/validUtils');
 const appError = require('../utils/appError');
 const { dataSource } = require('../db/data-source');
@@ -40,6 +41,49 @@ const collectionsController = {
         status: 'success',
         message: '刪除成功'
       });
+    } catch (error) {
+      next(error);
+    }
+  },
+  async addToCollection(req, res, next) {
+    const { collectionId } = req.params;
+    const { photoId } = req.body;
+    if (!isValidUUID(collectionId))
+      return next(appError(400, 'collection id 格式錯誤'));
+    if (!isValidUUID(photoId)) return next(appError(400, 'photo id 格式錯誤'));
+
+    try {
+      const collectionsRepo = dataSource.getRepository('Collections');
+      const collection = await collectionsRepo.findOneBy({
+        id: collectionId,
+        user: { id: req.user.id }
+      });
+
+      if (!collection) return next(appError(404, '查無此資料'));
+
+      const sharePhotosRepo = dataSource.getRepository('SharedPhotos');
+      const photo = await sharePhotosRepo.findOneBy({
+        id: photoId,
+        canceled_at: IsNull()
+      });
+
+      if (!photo) return next(appError(404, '查無此資料'));
+
+      const favoritesRepo = dataSource.getRepository('Favorites');
+      const data = await favoritesRepo
+        .save({
+          user: { id: req.user.id },
+          collections: { id: collectionId },
+          sharedPhotos: { id: photoId }
+        })
+        .catch((err) => {
+          if (err.code === '23505') {
+            throw appError(409, '不可重複加入');
+          }
+          throw err;
+        });
+
+      res.status(200).json({ status: 'success', data });
     } catch (error) {
       next(error);
     }
--- routes/collections.js
+++ routes/collections.js
@@ -3,6 +3,11 @@ const collectionsController = require('../controllers/collections');
 const isAuth = require('../middlewares/isAuth');
 const router = express.Router();
 
+router.post(
+  '/:collectionId/favorites',
+  isAuth,
+  collectionsController.addToCollection
+);
 router.delete('/:collectionId', isAuth, collectionsController.deleteCollection);
 router.post('/', isAuth, collectionsController.addCollection);
 
